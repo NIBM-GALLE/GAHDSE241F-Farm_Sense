489ce70f1f8191a5aaabb4beac45b764b8d68409
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-02 20:12:43 +0530

    install packages and create farmer model

---
+++ server/models/farmer.model.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const farmerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxLength: [100, "Name cannot be more than 100 characters"],
+      minLength: [5, "Name must be at least 5 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      max: 100,
+      unique: [true, "Email already exists"],
+      required: [true, "Please enter email"],
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter password"],
+      trim: true,
+      minLength: [6, "Password must be at least 6 characters"],
+      maxLength: [40, "Password cannot be more than 40 characters"],
+    },
+
+    address: {
+      type: String,
+      trim: true,
+      maxLength: [200, "Address cannot be more than 200 characters"],
+      minLength: [50, "Address must be at least 50 characters"],
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    phone: {
+      type: String,
+      maxLength: [15, "Phone number cannot be more than 15 characters"],
+      minLength: [10, "Phone number must be at least 10 characters"],
+      trim: true,
+    },
+
+    plants: {
+      name: [String],
+      maxLength: [100, "Plant name cannot be more than 100 characters"],
+      minLength: [5, "Plant name must be at least 5 characters"],
+    },
+
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+  { timestamps: true }
+);
+
+farmerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+farmerSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const Farmer = mongoose.model("Farmer", farmerSchema);
+export default Farmer;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
+  connectDB();
+});
+
+// error handle middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
