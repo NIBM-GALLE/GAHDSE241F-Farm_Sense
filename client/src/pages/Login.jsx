89710c94fbdda1aba11daeff05caba820a017c90
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-19 14:37:40 +0530

    create login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,102 @@
 import React from "react";
+import loginSvg from "../assets/images/login.png";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 function Login() {
-  return <div className="font-poppins text-2xl">Login</div>;
+  // schema for form validation
+  const formSchema = z.object({
+    email: z.string().email({
+      message: "Please enter a valid email address",
+    }),
+    password: z.string().min(4, {
+      message: "Please enter your password",
+    }),
+  });
+
+  // Initialize the form with react-hook-form and zod
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // Handle submission
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
+
+  return (
+    <div className="min-h-screen grid sm:grid-cols-2 mx-auto justify-center items-center px-4">
+      <div className="mx-auto">
+        <img src={loginSvg} alt="login" className="max-w-full h-auto" />
+      </div>
+
+      <div className="mx-auto w-full max-w-md">
+        <div className="text-center mb-6">
+          <h1 className="font-purple-purse text-4xl mb-2">Oh, You're back!</h1>
+          <h2 className="font-poppins text-2xl text-gray-600">
+            Log in with your credentials
+          </h2>
+        </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your Password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                type="submit"
+                variant="default"
+                size="lg"
+                className="w-full"
+              >
+                Login
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
