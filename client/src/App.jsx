eee755e03403facc0aad8939777596f29807c40e
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-03-21 13:33:16 +0530

    navbar create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,22 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Forget_password from "./pages/Forget_password";
 import Reset_password from "./pages/Reset_password";
+import Home from "./pages/Home";
+
+// App component
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forget-password" element={<Forget_password />} />
           <Route path="/reset-password" element={<Reset_password />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
