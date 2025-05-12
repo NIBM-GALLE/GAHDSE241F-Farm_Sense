4832bd82379dc5b30902905446715eb4791aca99
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2025-05-12 18:29:31 +0530

    rmv outside node modules

--- mobileapp/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AppNavigation from "../mobileapp/src/navigation/AppNavigation";
-
-const App = () => {
-  return (
-    <AppNavigation />
-  );
-};
-export default App;
